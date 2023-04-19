c85eb34415c6bbcda6a0bb5a8f5b2124e1227328
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-04-19 19:38:45 -0300

    ...

--- website/src/components/Posts/index.jsx
+++ website/src/components/Posts/index.jsx
@@ -3,9 +3,11 @@ import "./post.css";
 export default function Posts({ post }) {
   return (
     <div className="container-post">
-      <img src={post.img} alt="" />
-      <h2>{post.title}</h2>
-      <p>{post.describe}</p>
+      <img src={post.img} alt="exercícios" />
+      <div className="container-content">
+        <h2>{post.title}</h2>
+        <p>{post.describe}</p>
+      </div>
     </div>
   );
 }
--- website/src/pages/HomePage/index.jsx
+++ website/src/pages/HomePage/index.jsx
@@ -1,18 +1,29 @@
 import Posts from "../../components/Posts";
-import dados from '../../components/json/dados.json'
-import './HomePage.css'
+import dados from "../../components/json/dados.json";
+import "./HomePage.css";
+import benefits from "../../components/json/benefits.json";
+import img from "../../../src/assets/benefits.jpg";
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="container">
       <h2>Conheça 3 dos principais exercícios da calistenia</h2>
       <ul className="posts">
-        {dados.map(dado => (
-            <li key={dado.id}>
-                <Posts post={dado}/> 
-            </li>
+        {dados.map((dado) => (
+          <li key={dado.id}>
+            <Posts post={dado} />
+          </li>
         ))}
       </ul>
+      <div className="container-benefits">
+        <img src={img} alt="benefits" />
+        <ul>
+        <h2>Alguns benefícios da calistenia</h2>
+          {benefits.map((benefit) => (
+            <li key={benefit.id}>{benefit.benefit}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
