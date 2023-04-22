2f770e9ca25f09248ee17fa1b3419f09dae612df
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-04-22 14:38:18 -0300

    ...

--- website/src/components/Banner/index.jsx
+++ website/src/components/Banner/index.jsx
@@ -4,7 +4,9 @@ export default function Banner() {
   return (
     <div class="outer-text">
       <div class="text">
-        <span className="span-1">Descubra os limites do seu corpo e da sua mente</span>
+        <span className="span-1">
+          Descubra os limites do seu corpo e da sua mente
+        </span>
         <span>Fortaleça seu corpo, conheça a calistenia!</span>
         <button>Saiba mais</button>
       </div>
